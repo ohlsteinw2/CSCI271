6aea73d90f5babb3722cfa7e5d1f7fe5e96bf568
ohlsteinw2 <ohlsteinw2@winthrop.edu>
2026-02-05 17:07:16 -0500

    Added my methods from VS Code

--- Fraction/Fraction.java
+++ Fraction/Fraction.java
@@ -55,6 +55,36 @@ public class Fraction {
         }
         return numerator + "/" + denominator;
     }
+    public Fraction add(Fraction other) {
+        long num = this.numerator * other.denominator + other.numerator * this.denominator;
+        long denom = this.denominator * other.denominator;
+        return new Fraction(num, denom);
+    }
+    public Fraction subtract(Fraction other) {
+        long num = this.numerator * other.denominator - other.numerator * this.denominator;
+        long denom = this.denominator * other.denominator;
+        return new Fraction(num, denom);
+    }
+    public Fraction multiply(Fraction other) {
+        long num = this.numerator * other.numerator;
+        long denom = this.denominator * other.denominator;
+        return new Fraction(num, denom);
+    }
+    public Fraction divide(Fraction other) {
+        long num = this.numerator * other.denominator;
+        long denom = this.denominator * other.numerator;
+        return new Fraction(num, denom);
+    }
+
+    public Fraction negate() {
+        return new Fraction(-this.numerator, this.denominator);
+    }
+    public Fraction pow(int exponent) {
+        long num = (long) Math.pow(this.numerator, exponent);
+        long denom = (long) Math.pow(this.denominator, exponent);
+        return new Fraction(num, denom);
+    }
+
     public static void main (String[] args) {
         Fraction f1 = new Fraction (6, -24);
         System.out.println(f1);
@@ -67,5 +97,10 @@ public class Fraction {
         System.out.println(f6);
         Fraction f7 = new Fraction (0, 0);
 
+        Fraction f8 = new Fraction(16);
+        Fraction f9 = new Fraction(3, 5).add(new Fraction(7));
+        Fraction f10 = new Fraction(6,7);
+        Fraction results = f10.multiply(f8.divide(f9));
+        System.out.println(results);
     }
 }
