bee98c289db51a38135420726d9fa2b47b749c54
ohlsteinw2 <ohlsteinw2@winthrop.edu>
2026-02-22 22:21:16 -0500

    Made a working recursive call for question6 but not with array

--- Assignment3/Question6.java
+++ Assignment3/Question6.java
@@ -5,11 +5,15 @@ import java.util.ArrayList;
 public class Question6 {
     public static void main(String[] args){
         ArrayList<Integer> storeInt = new ArrayList<Integer>();
-        System.out.println("Enter integers and type q to quit.");
+        System.out.println("Enter a number");
         Scanner scanner = new Scanner (System.in);
-        while (!scanner.equals("q")){
-            int num;
-            storeInt.add(num);
+        int inputNum = scanner.nextInt();
+        System.out.println("Sum of numberes is " + sumEven(inputNum));
+    }
+    public static int sumEven(int inputNum){
+        if (inputNum <= 1){
+            return 1;
         }
+        return inputNum + sumEven(inputNum - 1);
     }
 }
