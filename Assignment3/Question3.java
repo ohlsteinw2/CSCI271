950a09b8dd8d93a8d1dc0e3a73c70db7cd549784
ohlsteinw2 <ohlsteinw2@mailbox.winthrop.edu>
2026-02-23 21:31:05 -0500

    Refactor main method and implement max function

--- Assignment3/Question3.java
+++ Assignment3/Question3.java
@@ -1,20 +1,35 @@
 package Assignment3;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Question3 {
-public static void main (String[] args){
-    int num;
-    String nums;
-    System.out.println("Enter some integers to add to our array!");
-    Scanner scanner = new Scanner(System.in);
-    if (scanner.equals("q")){
-        return;
+
+    public static void main (String[] args) {
+        int[] A = new int[10];
+        System.out.println("Enter ints to put into the array.");
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 0;
+        while (scanner.hasNextInt() && n < A.length) {
+            A[n] = scanner.nextInt();
+            n++;
+        }
+
+        System.out.println("List of array " + Arrays.toString(A));
+
+        if (n > 0) {
+            System.out.println("Max = " + max(A, n));
+        } else {
+            System.out.println("No numbers entered.");
+        }
     }
-    else { 
+
+    public static int max(int[] A, int n) {
+        if (n == 1) {          // base case: max of first 1 element
+            return A[0];
+        }
+
+        int prevMax = max(A, n - 1);  // recursive call
+        return (A[n - 1] > prevMax) ? A[n - 1] : prevMax;
     }
-     ArrayList<Integer> A = new ArrayList<Integer>(num);
-    }
-System.out.println("Our array of integers!" + (A));
-System.out.println("The maximum value is: ");
 }
